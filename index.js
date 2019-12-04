eb70c2c7e8406687dfa8b9863880a356ba2f5f5c
lucas miller <lucasmiller401@gmail.com>
2019-12-04 07:57:04 -0600

    update failure procedures

--- index.js
+++ index.js
@@ -24,15 +24,30 @@ app.post("/", function(req, res) {
   request('http://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q= ' + city, function(error, response, body) {
     var data = JSON.parse(body);
 
-
-    var city = data.location.name;
-    var temp = data.current.temp_f;
-    var time = data.location.localtime;
+    var statusCode = response.statusCode;
 
 
+    if(statusCode == 200){
+      var city = data.location.name;
+      var temp = data.current.temp_f;
+      var time = data.location.localtime;
+      var message1 = "I've heard ";
+      var message2 = "is a wonderful place! It is currently ";
+      var message3 = " in "
+      var message4 = ". The current date and time there is "
 
 
-    res.render('index', {city: city, temp: temp, time: time})
+      res.render('index2', {message1: message1, message2: message2, message3: message3, message4: message4, city: city, temp: temp, time: time})
+    } else{
+
+      var errMessage = "Please try a different zipcode";
+      res.render('index', {errMessage: errMessage, city: null, temp: null, time: null});
+    }
+
+
+
+
+
 
   });
 });
